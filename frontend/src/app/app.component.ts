d01730fa9fd645cdcb1d51a3aaf023ac89dcd936
huiyuxie <huiyuxie.sde@gmail.com>
2023-08-25 00:03:34 -1000

    test frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {untilDestroyed, UntilDestroy} from '@ngneat/until-destroy';
 
+enum Tab {
+  FRONTEND,
+  BACKEND
+}
+
+@UntilDestroy()
 @Component({
-  selector: 'app-root',
+  selector: 'app-component',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'frontend';
+  readonly tab = Tab;
+  currentTab: number = Tab.FRONTEND;
+  
+  constructor(
+    protected router: Router,
+  ) { }
+
+  ngOnInit() {
+    this.router.events.pipe(untilDestroyed(this)).subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.setActiveTab();  
+      }
+    }); 
+    this.setActiveTab();
+  }
+
+  setActiveTab(): void {
+    switch (true) {
+      case this.router.url.includes('player-summary'):
+        this.currentTab = Tab.FRONTEND;
+        break;
+      case this.router.url.includes('player-summary-api'):
+        this.currentTab = Tab.BACKEND;
+        break;
+    }
+  }
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {routing} from './app.routing';
+import {AppComponent} from './app.component';
+import {PlayersService} from './_services/players.service';
+
+import {CommonModule} from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon'
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSelectModule} from '@angular/material/select';
+import {MatOptionModule} from '@angular/material/core';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
@@ -10,9 +28,25 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatTabsModule,
+    routing
   ],
-  providers: [],
+  providers: [PlayersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
